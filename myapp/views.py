39acd8dc540612e689f02c25fd48ac8bf08eef28
OZ <wdr1020@naver.com>
2023-09-16 05:46:56 +0900

    읽기 구현 2-1

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome!')
+    return HttpResponse('''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            <li>routing</li>
+            <li>view</li>
+            <li>model</li>
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(request):
     return HttpResponse('Create!')
