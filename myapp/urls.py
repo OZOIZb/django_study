38e8c157bd41bff98f29b2bda545a94e6b01abc6
OZ <wdr1020@naver.com>
2023-09-16 02:54:47 +0900

    라우팅 1-3

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,5 +16,9 @@ Including another URLconf
 """
 
 from django.urls import path
+from myapp import views
 urlpatterns = [
+    path('', views.index),
+    path('create/', views.index),
+    path('read/1/', views.index)
 ]
