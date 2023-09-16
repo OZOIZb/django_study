18a830403af10e99b75c2cf97fa1859df7904182
OZ <wdr1020@naver.com>
2023-09-17 03:27:22 +0900

    삭제 구현 4-2

--- myapp/views.py
+++ myapp/views.py
@@ -8,7 +8,7 @@ topics = [
     {'id':3, 'title':'Model', 'body':'Model is ...'},
 ]
 
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag, id=None):
     global topics
     ol = ''
     for topic in topics:
@@ -25,6 +25,7 @@ def HTMLTemplate(articleTag):
             <li><a href="/create/">create</a></li>
             <li>
                 <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
                     <input type="submit" value="delete">
                 </form>
             </li>
@@ -46,7 +47,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
 def create(request):
